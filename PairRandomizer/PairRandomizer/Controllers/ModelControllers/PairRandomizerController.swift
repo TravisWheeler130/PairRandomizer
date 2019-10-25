4acb819d030d6c7b974b9e48a5f9a6420082f936
Travis Wheeler <travis.wheeler130@yahoo.com>
2019-10-25 09:57:29 -0600

    info.plist, model, and start of modelController

---
+++ PairRandomizer/PairRandomizer/Controllers/ModelControllers/PairRandomizerController.swift
@@ -0,0 +1,15 @@
+//
+//  PairRandomizerController.swift
+//  PairRandomizer
+//
+//  Created by Travis Wheeler on 10/25/19.
+//  Copyright © 2019 Travis Wheeler. All rights reserved.
+//
+
+import Foundation
+
+class PairRandomizerController {
+    
+    static let shared = PairRandomizerController()
+    
+}
---
+++ PairRandomizer/PairRandomizer/Models/PairRandomizer.swift
@@ -0,0 +1,18 @@
+//
+//  PairRandomizer.swift
+//  PairRandomizer
+//
+//  Created by Travis Wheeler on 10/25/19.
+//  Copyright © 2019 Travis Wheeler. All rights reserved.
+//
+
+import Foundation
+
+class PairRandomizer {
+    
+    var name: String
+    
+    init (name: String) {
+        self.name = name
+    }
+}
